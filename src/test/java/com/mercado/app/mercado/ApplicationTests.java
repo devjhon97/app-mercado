f05eea4691ad00f214b8680ef577b8ad9637f429
devjhon97 <jhonatanvideos337@gmail.com>
2024-03-06 14:12:14 -0300

    inicio projeto

---
+++ src/test/java/com/mercado/app/mercado/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado.app.mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
